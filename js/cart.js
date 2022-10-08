c9106b3be4a17867380d89f1b18fe1f6804b2172
bcorrealtez <brunocorreaaltez2016@gmail.com>
2022-10-08 12:33:33 -0300

    maquetado de lista finalizado, muestra de datos actualizada

--- js/cart.js
+++ js/cart.js
@@ -1,19 +1,39 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const lisCarrito = document.getElementById('listaCompra')
+function showCartList() {
+    let htmlConToAppend = '';
+    if (currentCartList) {
+        for (let i = 0; i < currentCartList.length; i++) {
+            htmlConToAppend += `
+            <div class="row rounded-3 p-2 bg-white shadow mb-2">
+                <div class="col-4 p-0 pe-2 my-auto">
+                    <img src=${currentCartList[i].image} alt="" class="img-fluid rounded">
+                </div>
+                <div class="col-8 ps-3">
+                    <div class="row">
+                        <h1>${currentCartList[i].name}</h1>
+                    </div>
+                    <div class="row align-items-center p-0">
+                        <div class="d-flex">
+                            <p class="mb-0 text-muted text-decoration-underline">Costo</p>
+                            <p class="mb-0">: ${currentCartList[i].currency} ${currentCartList[i].unitCost}</p>
+                        </div>
+                    </div>
+                    <div class="row d-flex mt-2">
+                        <div class="input-group">
+                            <span class="me-3 p-0 bg-white">${currentCartList[i].currency} ${currentCartList[i].unitCost * currentCartList[i].count}</span>
+                            <i class="fa fa-minus input-group-text d-flex px-1 py-0 rounded-start"></i>
+                            <input type="text" class="form-control p-1 py-0 border-start-0 text-center border-end-0" value="${currentCartList[i].count}" min="1"
+                                aria-label="Dollar amount">
+                            <i class="fa fa-plus input-group-text d-flex px-1 py-0"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+        lisCarrito.innerHTML = htmlConToAppend;
+    }
+}
 
 
 
